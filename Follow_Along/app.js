cdbe92ae706e15d34fd15c881728033622c95120
Remus1992 <Remington.Henderson@gmail.com>
2018-12-02 10:33:49 -0800

    Sending Debugging & Express

---
+++ Follow_Along/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const routes = require('./routes');
+
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
+
+const http = require('http');
+
+const routes = require('./routes');
+
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
+
