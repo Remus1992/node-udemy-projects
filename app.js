f84144e9fc2660218a9609e62ef78c664b8d0795
Remus1992 <Remington.Henderson@gmail.com>
2018-12-02 18:49:07 -0800

    Sending node/express updates

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
-const http = require('http');
-
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-// .use() allows us to add a new middleware function
-// it accepts an array of event handlers
-// building a function like the one below will be used on 
-// every incoming request
-app.use((req, res, next) => {
-    console.log("In the Middleware!");
-    // next() needs to be called for the next function to be called
-    next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((req, res, next) => {
-    console.log("In another Middleware!");
-    // functions like .setHeader() are still valid here
-    // but .send() is specific to express
-    res.send('<h1>Hello from Express!</h1>');
-});
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({extended: false}));
 
-server.listen(3000);
+app.use(adminRoutes);
+
+app.use(shopRoutes);
+
+app.listen(3000);
 
