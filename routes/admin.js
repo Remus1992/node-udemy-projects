cfb83f7654f87bb6cfb87bc87d08d9cda29eba57
Remus1992 <Remington.Henderson@gmail.com>
2018-12-02 21:25:42 -0800

    Sending Updates

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,19 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
+
+// the following two functions can have the same path 'url'
+// since they are running different functions 'get' vs 'post'
+// furthermore, both are implicitly reached at '/admin/add-product'
+// because they receive that argument 'filter' from app.js
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
