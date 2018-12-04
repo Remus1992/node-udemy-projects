5748910fc3eba40dddaf8bee0ec548be77a37383
Remus1992 <Remington.Henderson@gmail.com>
2018-12-04 12:18:07 -0800

    Sending PUG updates

--- app.js
+++ app.js
@@ -24,7 +24,10 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+
+    // this allows us to add dynamic content via pug (NOTE: '404.pug' is shortened)
+    res.render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,10 @@ const products = [];
 // furthermore, both are implicitly reached at '/admin/add-product'
 // because they receive that argument 'filter' from app.js
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    //res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+
+    // this allows us to add dynamic content via pug (NOTE: 'add-product.pug' is shortened)
+    res.render('add-product', {pageTitle: 'Add Product'});
 });
 
 router.post('/add-product', (req, res, next) => {
