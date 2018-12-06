8fd15e2485aeb4329593c67727da29df0e111160
Remus1992 <Remington.Henderson@gmail.com>
2018-12-06 11:10:36 -0800

    Sending MVC updates

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,8 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
-        pageTitle: 'Add Product', 
+        pageTitle: 'Add Product',
         path: '/admin/add-product',
         activeAddProduct: true,
         productCSS: true,
@@ -11,19 +11,20 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 }
 
-exports.getProducts = (req, res, next) => {    
-    // render() is provided by express and will use default 
-    // templating engine
-    res.render('shop', { 
-        prods : products, 
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-     });
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
+    });
 }
