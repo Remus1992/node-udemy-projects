bf442d3aef7e0763fa790df5700ed7286f60a4b7
Remus1992 <Remington.Henderson@gmail.com>
2018-12-12 12:05:55 -0800

    Sending Edit Product updates

--- routes/admin.js
+++ routes/admin.js
@@ -15,4 +15,5 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+router.get('/edit-product/:id', adminController.getEditProduct);
 module.exports = router;
