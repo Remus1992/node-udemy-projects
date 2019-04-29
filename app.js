3eb6a3e1a5edc53b995619df6793d513258e9da3
Remus1992 <Remington.Henderson@gmail.com>
2019-04-29 11:15:54 -0700

    Sending order model

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -46,10 +48,14 @@ Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem});
 Product.belongsToMany(Cart, { through: CartItem});
 
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
+
 sequelize
     // this will overwrite tables and isn't always wanted as it will delete previous information
-    // .sync({ force: true })
-    .sync()
+    .sync({ force: true })
+    // .sync()
     .then(result => {
         return User.findByPk(1)
         // console.log(result);
